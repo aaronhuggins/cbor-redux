208c3f1eb38958ee06b1921d1aa3b88c48a5dc12
Aaron Huggins <aaron@nuclearfamily.llc>
2020-09-02 21:32:13 -0500

    Add compiler support and remove grunt deps

--- gulpfile.ts
+++ gulpfile.ts
@@ -1,4 +1,13 @@
+import { rmdirSync } from 'fs'
 import * as shell from 'gulp-shell'
 
+// Tests
 export const mocha = shell.task(['mocha'])
 export const nyc = shell.task(['nyc mocha'])
+
+// Compiler
+export const tsc = shell.task(['tsc --sourceMap false', 'tsc --sourceMap false --module es2020 --outDir esm '])
+export const clean = async () => {
+  rmdirSync('./dist', { recursive: true })
+  rmdirSync('./esm', { recursive: true })
+}
