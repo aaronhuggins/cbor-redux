2120389e0548d578d69530d5762697350d208629
Aaron Huggins <aaron.huggins@gmail.com>
2020-09-03 13:19:22 -0500

    Enhance polluting the global scope

--- src/polyfill.ts
+++ src/polyfill.ts
@@ -1,5 +1,12 @@
 /** Method for polyfilling CBOR instead of intentionally importing. */
 export async function polyfill () {
-  // @ts-expect-error
-  window.CBOR = await import('./CBOR')
+  if (typeof window === 'object') {
+    // @ts-expect-error
+    window.CBOR = await import('./CBOR')
+  } else {
+    // @ts-expect-error
+    globalThis.CBOR = await import('./CBOR')
+  }
 }
+
+polyfill()
