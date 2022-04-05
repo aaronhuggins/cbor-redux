3e71666863b41fdddb165e8e6d5dbd63bf497992
Aaron Huggins <aaron.huggins@gmail.com>
2022-04-05 14:01:53 -0500

    Modify polyfill and provide expressive types

--- src/polyfill.ts
+++ src/polyfill.ts
@@ -1,12 +1,21 @@
+import type { CBOR as CBORImpl } from './CBOR.ts'
+
+declare global {
+  interface Window {
+    CBOR: typeof CBORImpl
+  }
+
+  let CBOR: typeof CBORImpl
+}
+
 /** Method for polyfilling CBOR instead of intentionally importing. */
 export async function polyfill () {
   if (typeof window === 'object') {
-    // @ts-expect-error
-    window.CBOR = await import('./CBOR')
+    window.CBOR = await import('./CBOR.ts')
   } else {
-    // @ts-expect-error
-    globalThis.CBOR = await import('./CBOR')
+    // @ts-expect-error: CBOR actually is declared on the global scope so we want this.
+    globalThis.CBOR = await import('./CBOR.ts')
   }
 }
 
-polyfill()
+await polyfill()
