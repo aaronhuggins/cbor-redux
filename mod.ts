a29c0013af8f77ceaa66bab4a3a2c80203d02495
Aaron Huggins <aaron.huggins@gmail.com>
2022-04-06 15:53:15 -0500

    Export symbols for empty keys and omitted values

--- mod.ts
+++ mod.ts
@@ -1,4 +1,5 @@
 export * from "./src/CBOR.ts";
+export { EMPTY_KEY, OMIT_VALUE } from "./src/constants.ts"
 export * from "./src/decode.ts";
 export * from "./src/encode.ts";
 export * from "./src/SimpleValue.ts";
