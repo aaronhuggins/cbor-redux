306f7da45bfc28f6e4f88a26d794c3c05c5450bd
Aaron Huggins <aaron.huggins@gmail.com>
2022-04-06 13:08:22 -0500

    Clean up exported API

--- src/CBOR.ts
+++ src/CBOR.ts
@@ -1,27 +1,14 @@
-// deno-lint-ignore-file no-explicit-any
 import { decode } from "./decode.ts";
 import { encode } from "./encode.ts";
-import { options } from "./helpers.ts";
-import type {
-  CBOROptions,
-  SimpleValueFunction,
-  TaggedValueFunction,
-} from "./types.ts";
 
 /**
  * An intrinsic object that provides functions to convert JavaScript values
  * to and from the Concise Binary Object Representation (CBOR) format.
  */
 export const CBOR: {
-  decode: <T = any>(
-    data: ArrayBuffer | SharedArrayBuffer,
-    tagger?: TaggedValueFunction,
-    simpleValue?: SimpleValueFunction,
-  ) => T;
-  encode: <T = any>(value: T) => ArrayBuffer;
-  options: (options?: CBOROptions) => Readonly<CBOROptions>;
+  decode: typeof decode;
+  encode: typeof encode;
 } = {
   decode,
   encode,
-  options,
 };
