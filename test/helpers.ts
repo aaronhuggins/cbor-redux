c07f1099ac5eb6882d6fb41a0f123f4c00ca9fb5
Aaron Huggins <aaron.huggins@gmail.com>
2022-04-05 20:58:42 -0500

    Clean up helpers, remove deprecated code

--- test/helpers.ts
+++ test/helpers.ts
@@ -4,44 +4,40 @@ import {
   ok,
 } from "https://deno.land/std@0.133.0/node/assert.ts";
 
-export function helpers() {
-  return {
-    myDeepEqual(actual: any, expected: any, message?: string) {
-      if (actual === expected) {
-        return true;
-      }
-      if (expected instanceof ArrayBuffer) {
-        expected = new Uint8Array(expected);
-      }
-      if (actual instanceof Uint8Array && expected instanceof Uint8Array) {
-        let bufferMatch = actual.length === expected.length;
+export function myDeepEqual(actual: any, expected: any, message?: string) {
+  if (actual === expected) {
+    return true;
+  }
+  if (expected instanceof ArrayBuffer) {
+    expected = new Uint8Array(expected);
+  }
+  if (actual instanceof Uint8Array && expected instanceof Uint8Array) {
+    let bufferMatch = actual.length === expected.length;
 
-        for (let i = 0; i < actual.length; i += 1) {
-          bufferMatch = bufferMatch && actual[i] === expected[i];
-        }
+    for (let i = 0; i < actual.length; i += 1) {
+      bufferMatch = bufferMatch && actual[i] === expected[i];
+    }
 
-        if (bufferMatch) return ok(true, message);
-      }
+    if (bufferMatch) return ok(true, message);
+  }
 
-      if (
-        typeof actual === "number" && typeof expected === "number" &&
-        Number.isNaN(expected)
-      ) {
-        return deepStrictEqual(Number.isNaN(actual), true, message);
-      }
+  if (
+    typeof actual === "number" && typeof expected === "number" &&
+    Number.isNaN(expected)
+  ) {
+    return deepStrictEqual(Number.isNaN(actual), true, message);
+  }
 
-      return deepStrictEqual(actual, expected, message);
-    },
+  return deepStrictEqual(actual, expected, message);
+}
 
-    hex2arrayBuffer(data: string) {
-      const length = data.length / 2;
-      const ret = new Uint8Array(length);
+export function hex2arrayBuffer(data: string) {
+  const length = data.length / 2;
+  const ret = new Uint8Array(length);
 
-      for (let i = 0; i < length; i += 1) {
-        ret[i] = parseInt(data.substr(i * 2, 2), 16);
-      }
+  for (let i = 0; i < length; i += 1) {
+    ret[i] = parseInt(data.substr(i * 2, 2), 16);
+  }
 
-      return ret.buffer;
-    },
-  };
+  return ret.buffer;
 }
