8d876038101d692bce65a554e70cc16e4693f705
Aaron Huggins <aaron.huggins@gmail.com>
2022-04-07 13:30:59 -0500

    Add parse and binarify to the intrinsic object

--- src/CBOR.ts
+++ src/CBOR.ts
@@ -1,14 +1,13 @@
-import { decode } from "./decode.ts";
-import { encode } from "./encode.ts";
+import { decode, parse } from "./decode.ts";
+import { encode, binarify } from "./encode.ts";
 
 /**
  * An intrinsic object that provides functions to convert JavaScript values
  * to and from the Concise Binary Object Representation (CBOR) format.
  */
-export const CBOR: {
-  decode: typeof decode;
-  encode: typeof encode;
-} = {
+export const CBOR = {
+  binarify,
   decode,
   encode,
+  parse,
 };
