807b61ef2d523651c24c1e3457e7505c499e7a20
Aaron Huggins <aaron.huggins@gmail.com>
2022-04-16 15:46:25 -0500

    Add explainer code block for CBOR variable

--- src/CBOR.ts
+++ src/CBOR.ts
@@ -4,6 +4,14 @@ import { binarify, encode } from "./encode.ts";
 /**
  * An intrinsic object that provides functions to convert JavaScript values
  * to and from the Concise Binary Object Representation (CBOR) format.
+ * 
+ * ```typescript
+ * // Simply a conveniently named-export.
+ * CBOR.binarify(...)
+ * CBOR.decode(...)
+ * CBOR.encode(...)
+ * CBOR.parse(...)
+ * ```
  */
 export const CBOR = {
   binarify,
