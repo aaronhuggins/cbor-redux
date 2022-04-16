e2daa7de9118035fd0b4e3c25cab571503784d6b
Aaron Huggins <aaron.huggins@gmail.com>
2022-04-16 12:51:28 -0500

    Provide reviver example in decoding

--- src/decode.ts
+++ src/decode.ts
@@ -28,6 +28,17 @@ import { CBOROptions, CBORReviver, CBORSequenceOptions } from "./types.ts";
  * console.log(decoded) // Expect: { "1": 2, "3": 4 }
  * ```
  * 
+ * Use custom handling with a reviver function - just like `JSON.parse`.
+ * 
+ * ```typescript
+ * const { buffer } = new Uint8Array([0xa1,0x63,0x75,0x72,0x6c,0xd8,0x20,0x70,0x68,0x74,0x74,0x70,0x3a,0x2f,0x2f,0x73,0x69,0x74,0x65,0x2e,0x63,0x6f,0x6d,0x2f])
+ * const decoded = decode(buffer, (key, value) => {
+ *   if (value instanceof TaggedValue && value.tag === 32) return new URL(value.value)
+ *   return value
+ * })
+ * console.log(decoded) // Expect: { url: URL { href: "http://site.com/" } }
+ * ```
+ * 
  * If maps which preserve the key data types are desired, use `dictionary: "map"`.
  * 
  * ```typescript
