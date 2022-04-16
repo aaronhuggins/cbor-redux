d15c97cd1f3c3358d2d8e5ea6eb97c039beaee04
Aaron Huggins <aaron.huggins@gmail.com>
2022-04-16 15:13:40 -0500

    Add doc examples for simple values class

--- src/SimpleValue.ts
+++ src/SimpleValue.ts
@@ -3,6 +3,24 @@
 /** Class for structuring a simple value. Unassigned or reserved simple values
  * are emitted as an instance of this class during decoding. This allows an
  * application to handle custom decoding in the `reviver` function of the decoder.
+ *
+ * ```typescript
+ * const { buffer } = new Uint8Array([0x81,0xf8,0x64])
+ * const decoded = decode(buffer, (key, value) => {
+ *   // Let's pretend that a simple value of 100 stands for JavaScript NaN
+ *   if (value instanceof SimpleValue && value.value === 100) return NaN
+ *   return value
+ * })
+ * console.log(decoded) // Expect: [ NaN ]
+ * ```
+ *
+ * Simple values can alsoe be encoded.
+ *
+ * ```typescript
+ * const simple = [new SimpleValue(100)]
+ * const encoded = encode(simple)
+ * console.log(new Uint8Array(encoded)) // Expect: Uint8Array(3) [ 129, 248, 100 ]
+ * ```
  */
 export class SimpleValue {
   static create(value: boolean | number | null | undefined) {
